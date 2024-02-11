b4db39b6732508526457fe5e8ad9eeba8f24f077
usuario <alu0101567211@ull.edu.es>
2024-02-11 12:01:07 +0000

    algoritmo y estructura de datos

---
+++ ayed_pt2/pr1/main_rational_t.cpp
@@ -0,0 +1,62 @@
+// AUTOR: 
+// FECHA: 
+// EMAIL: 
+// VERSION: 1.0
+// ASIGNATURA: Algoritmos y Estructuras de Datos
+// PRÁCTICA Nº: 1
+// COMENTARIOS: se indican entre [] las pautas de estilo aplicadas de
+//              "C++ Programming Style Guidelines"
+//              https://geosoft.no/development/cppstyle.html
+// COMPILACIÓN: g++ -g rational_t.cpp main_rational_t.cpp -o main_rational_t
+
+// pauta de estilo [92]: comentarios multilínea usando solo "//"
+
+#include <iostream>
+#include <cmath>
+
+// pauta de estilo [41]: ficheros de cabecera agrupados y separados
+#include "rational_t.hpp"
+
+using namespace std;
+
+int main()
+{
+  rational_t a(1, 2), b(3), c; //valores que se le pasan a la clase
+
+  cout << "a.value()= " << a.value() << endl;
+  cout << "b.value()= " << b.value() << endl;
+  cout << "c.value()= " << c.value() << endl;
+
+  // se contruyen las fracciones con los valores pasados
+  cout << "a: ";
+  a.write(); 
+  cout << "b: ";
+  b.write();
+  // se piden los valores para poder crear una tercera fraccion
+  c.read();
+  cout << "c: ";
+  c.write();
+
+  // FASE II
+  rational_t x(1, 3), y(2, 3);
+  x.write();
+  y.write();
+  cout << "x == y? " << (x.is_equal(y) ? "true" : "false") << endl;
+  cout << "x < y? " << (x.is_greater(y) ? "true" : "false") << endl;
+  cout << "x > y? " << (x.is_less(y) ? "true" : "false") << endl;
+
+  // FASE III
+  // cout << "a + b: ";
+  // a.add(b).write();
+  
+  // cout << "b - a: ";
+  // b.substract(a).write();
+
+  // cout << "a * b: ";
+  // a.multiply(b).write();
+  
+  // cout << "a / b: ";
+  // a.divide(b).write();
+  
+  return 0;
+}
---
+++ ayed_pt2/pr1/rational_t.hpp
@@ -0,0 +1,59 @@
+// AUTOR: Margarita Blanca Escobar Alonso
+// FECHA: 08/02/2024
+// EMAIL: alu0101567211@ull.edu.es
+// VERSION: 1.0
+// ASIGNATURA: Algoritmos y Estructuras de Datos
+// PRÁCTICA Nº: 1
+// COMENTARIOS: se indican entre [] las pautas de estilo aplicadas de
+//              "C++ Programming Style Guidelines"
+//              https://geosoft.no/development/cppstyle.html
+
+// pauta de estilo [92]: comentarios multilínea usando solo "//"
+
+#pragma once
+
+#include <iostream>
+#include <cassert>
+#include <cmath>
+
+// pauta de estilo [5]
+# define EPSILON 1e-6
+
+using namespace std;
+
+class rational_t
+{
+  // pautas de estilos [44] y [73]: primero "public" y después "private"
+public: // Elementos publicos
+  rational_t(const int = 0, const int = 1); //constructor
+  ~rational_t() {} //destructor
+  
+  // pauta de estilo [71]: indentación a 2 espacios
+  // getters
+  int get_num() const;
+  int get_den() const;
+  
+  // setters
+  void set_num(const int);
+  void set_den(const int);
+
+  double value(void) const; //metodo para obtener el valor rela de una fraccion
+
+  // FASE II
+  bool is_equal(const rational_t&, const double precision = EPSILON) const; //método para ver si es igual
+  bool is_greater(const rational_t&, const double precision = EPSILON) const; //método para ver si es mayor
+  bool is_less(const rational_t&, const double precision = EPSILON) const; //método para ver si es menor
+  
+  // FASE III
+  // rational_t add(const rational_t&);
+  // rational_t substract(const rational_t&);
+  // rational_t multiply(const rational_t&);
+  // rational_t divide(const rational_t&);
+  
+  void write(ostream& = cout) const; //lectura desde teclado  //ostream y cout llevan std::
+  void read(istream& = cin); //scritura a pantalla  // istream y cin llevan std::
+  
+private: // Elementos privados 
+  // pauta de estilo [11]: nombre de los atributos seguido de "_"
+  int num_, den_; // atributos
+};
